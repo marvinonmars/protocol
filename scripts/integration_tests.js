1104f1bb831f6d826772c7a681817c2a1932a1fd
Spike Spiegel <spikeonmars@protonmail.com>
2021-03-13 17:19:42 -0300

    Encode send msg attribute as binary

--- scripts/integration_tests.js
+++ scripts/integration_tests.js
@@ -48,33 +48,53 @@ async function test_deposit(terra, wallet, contract_address) {
   console.log(result);
 }
 
-async function test_redeem(terra, wallet, contract_address) {
+async function test_redeem(terra, wallet, lp_contract_address) {
   let reserve_query_msg = {"reserve": {"symbol": "luna"}};
-  let { ma_token_address } = await query_contract(terra, contract_address, reserve_query_msg);
+  let { ma_token_address } = await query_contract(terra, lp_contract_address, reserve_query_msg);
 
-  const receive_msg = {
-    "redeem":
-    {"id": "luna"}
-  }
+  /*
+  const execute_msg = {
+    "send": {
+      "contract": lp_contract_address,
+      "amount": "500",
+      "msg": { 
+        "receive_msg": { 
+          "redeem": {
+            "id": "luna"
+          }
+        }
+      }
+    }
+  };
+  */
+  const execute_msg = {
+    "send": {
+      "contract": lp_contract_address,
+      "amount": "500",
+      "msg": toEncodedBinary({ "redeem": {"id": "luna"} }),  
+    }
+  };
 
-  const execute_msg = {"send": {"contract": contract_address, "amount": 500, receive_msg}}
   const send_msg = new MsgExecuteContract(wallet.key.accAddress, ma_token_address, execute_msg);
-  await perform_transaction(terra, wallet, send_msg);
+  const res = await perform_transaction(terra, wallet, send_msg);
 
-  // const balance_query_msg = {"balance": {"address": wallet.key.accAddress}};
-  // let result = await query_contract(terra, ma_token_address, balance_query_msg);
-  // console.log(result);
+  const balance_query_msg = {"balance": {"address": wallet.key.accAddress}};
+  let result = await query_contract(terra, ma_token_address, balance_query_msg);
+  console.log(result);
   // verify ma_balance is 500 and uluna is 500 (converted 1:1)
+}
+
+function toEncodedBinary(object) {
+  return Buffer.from(JSON.stringify(object)).toString('base64');
 }
 
 const terra = new LocalTerra();
 const wallet = terra.wallets.test1;
-// const result = await deploy(terra, wallet);
-// const lp_contract_address = result.lp_contract_address;
-const lp_contract_address = 'terra13urdf0tt35xhlzna4kw4l05cfjpcxlphc75cgt';
+const deploy_result = await deploy(terra, wallet);
+const lp_contract_address = deploy_result;
 
-// await test_reserve_query(terra, lp_contract_address, "usd")
-// await test_reserve_query(terra, lp_contract_address, "luna");
-// await test_config_query(terra, lp_contract_address);
-// await test_deposit(terra, wallet, lp_contract_address);
+await test_reserve_query(terra, lp_contract_address, "usd")
+await test_reserve_query(terra, lp_contract_address, "luna");
+await test_config_query(terra, lp_contract_address);
+await test_deposit(terra, wallet, lp_contract_address);
 await test_redeem(terra, wallet, lp_contract_address);
